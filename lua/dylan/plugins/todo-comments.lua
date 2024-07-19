4c6c43017af2ed6cd29b6ce59e355353422f3cd5
DDuunk <dylanduunk@live.nl>
2024-07-20 01:38:18 +0200

    feat: update commenting plugins

--- lua/dylan/plugins/todo-comments.lua
+++ lua/dylan/plugins/todo-comments.lua
@@ -1,8 +1,7 @@
+-- Highlight todo, notes, etc in comments
 return {
   "folke/todo-comments.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "nvim-lua/plenary.nvim", },
-  config = function()
-    require("todo-comments").setup()
-  end,
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = { signs = false },
 }
--- lua/dylan/plugins/ts-comments.lua
+++ lua/dylan/plugins/ts-comments.lua
@@ -1,8 +1,7 @@
+-- Enhance Neovim's native commenting functionality
 return {
   "folke/ts-comments.nvim",
   event = "VeryLazy",
   enabled = vim.fn.has("nvim-0.10.0") == 1,
-  config = function()
-    vim.keymap.set({ "n", "v" }, "<C-_>", "gcc", { remap = true })
-  end
+  opts = {},
 }
