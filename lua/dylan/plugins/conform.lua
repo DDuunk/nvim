6d2530b6cf3d2d138ad719471b80191edbb44586
DDuunk <dylanduunk@live.nl>
2024-07-20 02:09:58 +0200

    feat: implement conform.nvim instead of none-ls.nvim

---
+++ lua/dylan/plugins/conform.lua
@@ -0,0 +1,54 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre", "BufNewFIle" },
+  keys = {
+    {
+      "<leader>gf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      log_level = vim.log.levels.INFO,
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { "stylua" },
+        typescript = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+        javascript = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        graphql = { "prettierd", "prettier" },
+        go = { "goimports", "gofmt" },
+        rust = { "rustfmt" },
+
+        ["*"] = { "codespell" },
+        -- Use the "_" filetype to run formatters on filetypes that don't
+        -- have other formatters configured.
+        ["_"] = { "trim_whitespace" },
+      },
+    })
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        require("conform").format({ bufnr = args.buf, lsp_fallback = true })
+      end,
+    })
+  end,
+}
--- lua/dylan/plugins/none-ls.lua
+++ lua/dylan/plugins/none-ls.lua
@@ -1,46 +1,21 @@
+-- @TODO: Testing out conform, might just delete this
 return {
-	-- "nvimtools/none-ls.nvim",
-	-- dependencies = { "nvim-lua/plenary.nvim" },
-	-- config = function()
-	-- 	local null_ls = require("null-ls")
-	--
-	-- 	null_ls.setup({
-	-- 		sources = {
-	-- 			null_ls.builtins.diagnostics.editorconfig_checker,
-	-- 			null_ls.builtins.formatting.stylua,
-	-- 			null_ls.builtins.formatting.prettier.with({
-	-- 				extra_filetypes = { "mdx" },
-	-- 			}),
-	-- 			null_ls.builtins.completion.spell,
-	-- 		},
-	-- 	})
-	-- end,
-	--
-	"jay-babu/mason-null-ls.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{ "williamboman/mason.nvim", opts = {} },
-		"nvimtools/none-ls.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-
-		require("mason-null-ls").setup({
-			ensure_installed = {
-				-- Opt to list sources here, when available in mason.
-				"editorconfig_checker",
-				"stylua",
-				"prettier",
-			},
-			automatic_installation = false,
-			handlers = {},
-		})
-
-		null_ls.setup({
-			sources = {
-				-- Anything not supported by mason.
-				null_ls.builtins.completion.spell,
-			},
-		})
-	end,
+  -- "nvimtools/none-ls.nvim",
+  -- config = function()
+  --   -- local null_ls = require("null-ls")
+  --   --
+  --   -- null_ls.setup({
+  --   --   sources = {
+  --   --     null_ls.builtins.formatting.rustfmt.with({
+  --   --       condition = function(utils)
+  --   --         return utils.root_has_file("Cargo.toml")
+  --   --       end,
+  --   --     }),
+  --   --     null_ls.builtins.formatting.stylua,
+  --   --     null_ls.builtins.formatting.prettier,
+  --   --     null_ls.builtins.diagnostics.editorconfig_checker,
+  --   --     null_ls.builtins.diagnostics.eslint_d,
+  --   --   },
+  --   -- })
+  -- end,
 }
