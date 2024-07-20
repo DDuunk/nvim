b6b717ff7d49204043352cc2a68a88b76c468d72
DDuunk <dylanduunk@live.nl>
2024-07-20 02:06:16 +0200

    feat: extend auto commands

--- lua/dylan/core/autocmd.lua
+++ lua/dylan/core/autocmd.lua
@@ -1,41 +1,58 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local formatGroup = augroup("FormatGroup", { clear = true })
+
+-- Add custom filetype for mdx files
+vim.filetype.add({
+  extension = {
+    mdx = "mdx",
+  },
+})
+
+-- Disable new line comment
+autocmd("BufEnter", {
+  group = formatGroup,
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
+-- Remove trailing whitespace
+autocmd("BufWritePre", {
+  group = formatGroup,
+  command = [[%s/\s\+$//e]],
+  pattern = "*",
+})
+
+-- Enable spell checking on specific filetypes
+autocmd("BufEnter", {
+  group = augroup("SpellCheck", { clear = true }),
+  pattern = { "*.md", "*.txt" },
+  callback = function()
+    vim.opt_local.spell = true
+  end,
+})
+
 -- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
+autocmd("TextYankPost", {
+  group = augroup("HighlightOnYank", { clear = true }),
   pattern = "*",
   callback = function()
     vim.highlight.on_yank({
-      higroup = 'IncSearch',
+      higroup = "IncSearch",
       timeout = 40,
     })
   end,
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("DylanDuunk", {}),
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
-})
-
-vim.api.nvim_create_autocmd("QuitPre", {
-  group = vim.api.nvim_create_augroup("CloseLoclistWindowGroup", { clear = true }),
+-- Close loclist window on quit if buffer is empty
+autocmd("QuitPre", {
+  group = augroup("CloseLoclistWindowGroup", { clear = true }),
   pattern = "*",
   callback = function()
-    if vim.bo.buftype == '' then
-      vim.cmd('lclose')
+    if vim.bo.buftype == "" then
+      vim.cmd("lclose")
     end
-  end
-})
-
--- vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
---
--- vim.api.nvim_create_autocmd("User", {
---   group = "lualine_augroup",
---   pattern = "LspProgressStatusUpdated",
---   callback = require("lualine").refresh,
--- })
-
-vim.filetype.add({
-  extension = {
-    mdx = "mdx",
-  },
+  end,
 })
